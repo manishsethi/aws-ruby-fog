eb0678c86699370e399cd676024810b4abf29a7a
Manish Sethi <manish@sethis.in>
2015-06-17 17:14:54 +0000

    added ruby script to stop and start the instance

---
+++ start-stop.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require 'fog'
+
+$inst='something'
+
+
+connection = Fog::Compute.new({:provider => 'AWS'})
+puts "Shutting down the instance #{$inst}"
+
+connection.stop_instances("#{$inst}")
+puts "Waiting for instance to stop"
+sleep(20)
+
+puts "Starting the instance #{$inst}"
+
+connection.start_instances("#{$inst}")
+
